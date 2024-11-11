45f057becec7089da515a379779e287a18cd7987
mchen127 <mchen127.p@gmail.com>
2024-11-11 21:07:06 +0800

    feat: add docker-compose file

--- app/(protected)/recommendation/[recommendation_id]/page.tsx
+++ app/(protected)/recommendation/[recommendation_id]/page.tsx
@@ -12,7 +12,7 @@ const RecommendationPage = async ({
   params,
   searchParams,
 }: {
-  params: { recommendation_id: number };
+  params: { recommendation_id: string };
   searchParams?: { [key: string]: string | undefined };
   }) => {
   const supabase = createClient();
