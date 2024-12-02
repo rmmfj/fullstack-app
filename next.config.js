414afe3a9d3f85ef68719a48ab36b5213be5f407
mchen127 <mchen127.p@gmail.com>
2024-12-02 18:21:54 +0800

    time

--- next.config.js
+++ next.config.js
@@ -39,65 +39,65 @@ const nextConfig = {
         hostname: "www.50-shop.com",
       },
       {
-        protocol: 'https',
-        hostname: 'lp2.hm.com',
+        protocol: "https",
+        hostname: "lp2.hm.com",
       },
       {
-        protocol: 'https',
-        hostname: 'www.gu-global.com',
+        protocol: "https",
+        hostname: "www.gu-global.com",
       },
       {
-        protocol: 'https',
-        hostname: 'www.lativ.com.tw',
+        protocol: "https",
+        hostname: "www.lativ.com.tw",
       },
       {
-        protocol: 'https',
-        hostname: 's.lativ.com.tw',
+        protocol: "https",
+        hostname: "s.lativ.com.tw",
       },
       {
-        protocol: 'https',
-        hostname: 'www.meierq.com',
+        protocol: "https",
+        hostname: "www.meierq.com",
       },
       {
-        protocol: 'https',
-        hostname: 'pics.meierq.com',
+        protocol: "https",
+        hostname: "pics.meierq.com",
       },
       {
-        protocol: 'https',
-        hostname: 'www.net-fashion.net',
+        protocol: "https",
+        hostname: "www.net-fashion.net",
       },
       {
-        protocol: 'https',
-        hostname: 'net.wimg.tw',
+        protocol: "https",
+        hostname: "net.wimg.tw",
       },
       {
-        protocol: 'https',
-        hostname: 'www.obdesign.com.tw',
+        protocol: "https",
+        hostname: "www.obdesign.com.tw",
       },
       {
-        protocol: 'https',
-        hostname: 'diz36nn4q02zr.cloudfront.net',
+        protocol: "https",
+        hostname: "diz36nn4q02zr.cloudfront.net",
       },
       {
-        protocol: 'https',
-        hostname: 'www.pazzo.com.tw',
+        protocol: "https",
+        hostname: "www.pazzo.com.tw",
       },
       {
-        protocol: 'https',
-        hostname: 'pics.pzcdn.tw',
+        protocol: "https",
+        hostname: "pics.pzcdn.tw",
       },
       {
-        protocol: 'https',
-        hostname: 'www.zara.com',
+        protocol: "https",
+        hostname: "www.zara.com",
       },
       {
-        protocol: 'https',
-        hostname: 'static.zara.net',
+        protocol: "https",
+        hostname: "static.zara.net",
       },
       {
-        protocol: 'https',
-        hostname: 'www.edwin.com.tw',
-      }
+        protocol: "https",
+        hostname: "www.edwin.com.tw",
+      },
     ],
   },
   experimental: {
@@ -106,15 +106,18 @@ const nextConfig = {
     },
   },
   webpack: (config, { isServer }) => {
-    console.log(`LOGGING_ENABLED: ${process.env.LOGGING_ENABLED}`)
+    console.log(
+      `LOGGING_ENABLED: ${process.env.LOGGING_ENABLED}, type: ${typeof process
+        .env.LOGGING_ENABLED}`
+    );
     if (process.env.LOGGING_ENABLED === "true") {
       console.log = function (message) {
         process.stdout.write(message + "\n");
       };
     } else {
-      console.log = function (message) { };
+      console.log = function (message) {};
     }
-      return config;
+    return config;
   },
 };
 
